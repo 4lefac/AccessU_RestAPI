2197118cb32127865b0e4b0a4944118b5561359b
Forlefac Fontem <fontem.2@osu.edu>
2019-03-22 00:57:53 -0400

    patching complete

--- functions/api/v2/routes/images.js
+++ functions/api/v2/routes/images.js
@@ -1,72 +1,81 @@
+/*
+ * Setting up variables
+ */
 const express = require('express');
 const router = express.Router();
 const os = require('os');
 const path = require('path');
-
-//for handling the files
 const Busboy = require('busboy');
 const fs = require('fs');
-
-//can be found on the storage website
 const defaultBucketName = ('accessu-c0933.appspot.com');
 const admin = require("firebase-admin");
-
-//storage stuff
 const gcs = admin.storage();
 const bucket = gcs.bucket(defaultBucketName);
-
-//getting the DB reference
 var db = admin.firestore();
-var entrances = new Array();
 var entrancesRef = db.collection('entrances');
 var locationsRef = db.collection('locations');
 
-//check if image name is correct. If the image name is correct then it will add it to the file.
-function checkIfImageNameIsCorrectLocation(name,req,res){
-    var ans = true;
-    locationsRef.doc(name).get()
-    .then(doc => {
-            //checking if doc exist
-            console.log('checked');
-        if (!doc.exists) {
-            ans = false;
-        }
-    }).then(() => {
-        //if exist add the file else send a message saying it doesn't exist
-        if(ans){
-            addFile(name, req, res);
-        } else {
-            res.status(400).json({
-                message : "invalid id, please double check",
-            })
-        }
-    })
-    .catch((err) => {
-        res.status(400).json({
-            message : "error getting location reference",
-            error : err
+/*
+ * The purpose of this promise is to check if the image name is valid object in either one of our collections (locations or entrances).
+ * WIll resolve to true if the file exist or it will reject and send an error if it does not exist.
+ */
+function check (id, collection){
+    return new Promise((resolve,reject) => {
+    /*
+    *  getting the location id and if the location id cant be found then send an error to the user
+    */ 
+   if(collection === 'locations'){
+        locationsRef.doc(id).get().then(doc => {
+            if (!doc.exists) {
+                reject("file not found, please make sure location id is correct");
+            } else {
+                resolve(true);
+            }
         })
-    });
+        .catch((err) => {
+            reject(err)
+        });
+    } 
+    else {
+        entrancesRef.doc(id).get().then(doc => {
+            if (!doc.exists) {
+                reject("file not found, please make sure entrance id is correct");
+            } else {
+                resolve(true);
+            }
+        })
+        .catch((err) => {
+            reject(err)
+        });
+    }
+    })
 }
 
-function addFile (imageName, req, res){
-    //busboy allows us the parse the form data
+/*
+ * promise adds the file to the database and sends download url back
+ */
+function add (imageName, req){
+    return new Promise((resolve, reject) => {
+            //busboy allows us the parse the form data
     const busboy = new Busboy({ headers: req.headers });
     let uploadData = null;
-    //parsing a file
+
+    /*
+    * getting file from the api call
+    */
     busboy.on("file", (fieldname, file, filename, encoding, mimetype) => {
         const filepath = path.join(os.tmpdir(), filename);
         uploadData = { file: filepath, type: mimetype };
-  //actually storing the file in a temporary folder. whenver a file is downloaded it goes to a temporary location
-    file.pipe(fs.createWriteStream(filepath));
+        //actually storing the file in a temporary folder. whenver a file is downloaded it goes to a temporary location
+        file.pipe(fs.createWriteStream(filepath));
     });
 
-    //uupload to the server
+    /*
+    * uploading to the server
+    */
     busboy.on("finish", () => {
         //once parse we upload it to the initialized google cloud storage package
-        console.log(uploadData.file);
-        bucket
-        .upload(uploadData.file, {
+        bucket.upload(uploadData.file, {
             uploadType: "media",
             metadata: {
                 metadata: {
@@ -74,30 +83,43 @@ function addFile (imageName, req, res){
                 }
             },
             destination: ('/images/' + imageName)
-        })
-        .then((data) => {
+        }).then((data) => {
             //getting the image URL
             let file = data[0];
             file.getSignedUrl({action: 'read', expires: '03-09-2491'}).then(urls => {
                 const signedUrl = urls[0];
-                console.log(signedUrl);
+                resolve(signedUrl);
             }).catch (err => {
-                console.log(err);
+                reject(err);
             })
-        }).then(() => {
-            //sending to the user it worked
-            res.status(200).json({
-                message: "It worked!"
-            });
-        })
-        .catch(err => {
-            res.status(500).json({
-            error: err
-            });
+        }).catch(err => {
+            reject(err);
         });
     });
     //ending the process
     busboy.end(req.rawBody);
+    })
+}
+
+/*
+ * updates the imageUrl property for the entrance or location with the sepcidic id. collections can be locations or entrances
+ */
+function updateUrl(id, collection, signedUrl){
+    return new Promise ((resovle, reject) => {
+        if(collection === 'locations'){       
+        locationsRef.doc(id).update({
+            imageUri: signedUrl
+        }).catch((err) => {
+            reject(err);
+        })
+        } else {
+            entrancesRef.doc(id).update({
+                imageUri: signedUrl
+            }).catch((err) => {
+                reject(err);
+            })
+        }
+    })
 }
 
 router.post('/location/:imageName', (req, res, next) => {
@@ -105,6 +127,36 @@ router.post('/location/:imageName', (req, res, next) => {
     var name = req.params.imageName;
 
     //check if image name is correct and if it is correct then add it to hte file and send a response to the user.
-    checkIfImageNameIsCorrectLocation(name,req,res);
+    check(name, 'locations').then(() => {
+        return add(name,req);
+    }).then((signedUrl) => {
+        updateUrl(name,'locations', signedUrl)
+    }).then(() => {
+        res.status(200).send('success');
+    }).catch((err) => {
+        res.status(200).json({
+            message: 'error here',
+            error: err
+        })
+    })
+});
+
+router.post('/entrance/:imageName', (req, res, next) => {
+    //image name
+    var name = req.params.imageName;
+
+    //check if image name is correct and if it is correct then add it to hte file and send a response to the user.
+    check(name, 'entrances').then(() => {
+        return add(name,req);
+    }).then((signedUrl) => {
+        updateUrl(name,'entrances', signedUrl)
+    }).then(() => {
+        res.status(200).send('success');
+    }).catch((err) => {
+        res.status(200).json({
+            message: 'error here',
+            error: err
+        })
+    })
 });
 module.exports  = router;
